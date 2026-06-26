8f28be52d39078821c29a7ae73c6019c57273a38
Bharatpathania7 <studiopathania@gmail.com>
2026-06-26 15:17:31 +0530

    new changes

--- src/main/java/Shell_scripting/demo/DemoApplication.java
+++ src/main/java/Shell_scripting/demo/DemoApplication.java
@@ -5,10 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private HealthCheckService healthCheckService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+
+		boolean status = healthCheckService.checkHealth();
+
+		if (status) {
+			System.exit(0);
+		} else {
+			System.exit(1);
+		}
+	}
 }
--- src/main/java/Shell_scripting/demo/service/HealthCheckService.java
+++ src/main/java/Shell_scripting/demo/service/HealthCheckService.java
@@ -1,10 +1,8 @@
 package Shell_scripting.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,41 +12,30 @@ public class HealthCheckService {
     @Value("${monitor.url}")
     private String url;
 
-    @Autowired
-    private EmailService emailService;
-
     private final RestTemplate restTemplate = new RestTemplate();
 
-    @Scheduled(fixedRateString = "${monitor.interval}")
-    public void checkHealth() {
+    public boolean checkHealth() {
 
         try {
 
             ResponseEntity<String> response =
                     restTemplate.getForEntity(url, String.class);
 
-            if(response.getStatusCode() == HttpStatus.OK){
+            if (response.getStatusCode() == HttpStatus.OK) {
 
-                System.out.println("Application Healthy");
+                System.out.println("✅ Application is Healthy");
+                return true;
 
-            }else{
+            } else {
 
-                emailService.sendAlert(
-                        "Application Down",
-                        "Status Code : " + response.getStatusCode()
-                );
-
+                System.out.println("❌ Application returned : " + response.getStatusCode());
+                return false;
             }
 
-        } catch (Exception ex){
+        } catch (Exception ex) {
 
-            emailService.sendAlert(
-                    "Application Down",
-                    ex.getMessage()
-            );
-
+            System.out.println("❌ Health Check Failed : " + ex.getMessage());
+            return false;
         }
-
     }
-
 }
