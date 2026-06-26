e091842858bdbdc1720f0d6750bc2caab139f252
Bharatpathania7 <studiopathania@gmail.com>
2026-06-26 15:19:12 +0530

    new changes   ll

--- src/main/java/Shell_scripting/demo/DemoApplication.java
+++ src/main/java/Shell_scripting/demo/DemoApplication.java
@@ -1,8 +1,10 @@
 package Shell_scripting.demo;
 
+import Shell_scripting.demo.service.HealthCheckService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
